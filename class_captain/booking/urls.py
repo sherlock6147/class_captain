c2aab8d18aa31991030b3797e89a8ce0c9c57ebe
“sherlock6147” <tooshort9541@gmail.com>
2023-11-02 18:29:43 +0530

    added vview all bookings page

--- class_captain/booking/urls.py
+++ class_captain/booking/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from booking.views import (
     list_all,
     add_booking,
-    edit_booking,
     approve_booking,
     delete_booking,
     detail_view,
@@ -13,7 +12,6 @@ urlpatterns = [
     path('', list_all, name="list_all"),
     path('add/<classroom_id>',add_booking, name='add'),
     path('delete/<booking_id>',view=delete_booking,name='delete'),
-    path('edit/<booking_id>',view=edit_booking, name='edit'),
     path('approve/<booking_id>',view=approve_booking, name='approve'),
     path('view/classroom/<classroom_id>',view=detail_view,name="classroom_booking_detail")
 ]
--- class_captain/booking/views.py
+++ class_captain/booking/views.py
@@ -36,7 +36,38 @@ def detail_view(request, classroom_id):
     return render(request, 'pages/view_classroom.html',context)
 
 def list_all(request):
-    ...
+    context = {}
+    context = prepare_context(request)
+    is_professor = context.get("is_professor", False)
+    is_student = context.get("is_student", False)
+    is_admin = context.get("is_admin", False)
+    if not(is_admin or is_professor or is_student):
+        return redirect('base:profile')
+    bookings = []
+    if is_admin:
+        bookings = Booking.objects.all()
+    if is_professor or is_student:
+        valid_classrooms = get_classrooms_for_user(request)
+        for room in valid_classrooms:
+            room_bookings = Booking.objects.filter(classroom=room)
+            if room_bookings.exists():
+                for bk in room_bookings:
+                    bookings.append(bk)
+    new_bookings = []
+    if is_student:
+        student = Student.objects.get(user=request.user)
+        for bk in bookings:
+            new_booking = bk
+            if bk.suggested_by == student:
+                new_booking.can_delete = True
+            else:
+                new_booking.can_delete = False
+            new_bookings.append(new_booking)
+    else:
+        new_bookings = bookings
+    context['bookings'] = new_bookings
+    print(context)
+    return render(request,'pages/bookings.html',context)
 
 @login_required
 def add_booking(request, classroom_id):
@@ -94,11 +125,29 @@ def add_booking(request, classroom_id):
                     messages.error(request, 'Conflicts with existing bookings')
     return render(request, 'pages/add_booking.html',context)
 
-def edit_booking(request, classroom_id):
-    ...
-
 def delete_booking(request, booking_id):
-    ...
+    context = {}
+    context = prepare_context(request)
+    is_professor = context.get("is_professor", False)
+    is_student = context.get("is_student", False)
+    is_admin = context.get("is_admin", False)
+    booking = Booking.objects.get(id=booking_id)
+    classroom = booking.classroom
+    if is_student:
+        student = Student.objects.get(user=request.user)
+        if booking.suggested_by == student:
+            booking.delete()
+            messages.success(request,'Deleted booking by student')
+    if is_professor:
+        # prof = Professor.objects.get(user=request.user)
+        classrooms = get_classrooms_for_user(request)
+        if classroom in classrooms:
+            booking.delete()
+            messages.success(request,'Deleted Booking by Professor')
+    if is_admin:
+        booking.delete()
+        messages.success(request,'Deleted Booking by Admin')
+    return redirect('booking:list_all')
 
 @login_required
 def approve_booking(request, booking_id):
